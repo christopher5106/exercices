82b145fd1bf24c49255fff36729ac5b5307cf77d
chris <cbo@ivalua.com>
2019-03-21 19:16:47 +0100

    n

--- cntk-ex.py
+++ cntk-ex.py
@@ -8,16 +8,16 @@ labels[X[:, 0] > X[:,1]] = [0,0,1]
 labels[X[:, 0] <= X[:,1]] = [1,0,0]
 labels[X[:,1] + X[:, 0] > 1] = [0, 1, 0]
 
-x = C.input_variable(shape=(2,), needs_gradient=False)
-t = C.input_variable(shape=(3,), needs_gradient=False)
+x = C.input_variable(shape=(-1, 2,), needs_gradient=False)
+t = C.input_variable(shape=(-1, 3,), needs_gradient=False)
 
 init = C.initializer.normal(0.01)
 
 theta1 = C.Parameter(shape=(2, 12), init=init )
-bias1 = C.Parameter(shape=(12,), init=init )
+bias1 = C.Parameter(shape=(1, 12), init=init )
 
 theta2 = C.Parameter(shape=(12,3), init=init )
-bias2 = C.Parameter(shape=(3,), init=init )
+bias2 = C.Parameter(shape=(1, 3,), init=init )
 
 def forward(x):
     y = C.times(x, theta1) + bias1
@@ -26,17 +26,13 @@ def forward(x):
 
 def softmax(x):
     e = C.exp(x)
-    s = C.reduce_sum(e)
+    s = C.reduce_sum(e, axis=1)
     return e/s
 
 def crossentropy(y, t):
-    prob = C.reduce_sum(y*t)
-    return -C.log(prob)
+    prob = C.squeeze(C.reduce_sum(y*t, axis=1), 1)
+    return - C.reduce_mean(C.log(prob))
 
-# print(forward(x).shape)
-# print((C.times(x, theta1)).eval({x:X[0:10],t:labels[:10]}))
-# import sys
-# sys.exit(0)
 #y = C.reduce_mean(C.cross_entropy_with_softmax(forward(x), t, axis=1))
 y = crossentropy(softmax(forward(x)),t)
 
@@ -51,12 +47,12 @@ for i in range(min(dataset_size, 100000) // batch_size ):
     loss = y.eval({x:sample, t:target})
     print("cost {} - learning rate {}".format(loss[0], lr))
 
-y = C.argmax(forward(x))
+y = C.squeeze(C.argmax(forward(x), 1),1)
 accuracy = 0
 for i in range(1000):
     sample = X[batch_size*i:batch_size*(i+1)]
     target = labels[batch_size*i:batch_size*(i+1)]
-    tt = y.eval({x:sample})
+    tt = y.eval({x:sample})[0]
     accuracy += np.sum(tt == np.argmax(target, axis=1))
 
 print("Accuracy", accuracy / 1000. /batch_size)
