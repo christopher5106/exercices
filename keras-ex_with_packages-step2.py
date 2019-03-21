3641891cf05a4c244152e1e56371cef4dcd532ef
chris <cbo@ivalua.com>
2019-03-21 10:59:52 +0100

    n

--- keras-ex_with_packages-step2.py
+++ keras-ex_with_packages-step2.py
@@ -12,9 +12,6 @@ labels[X[:, 0] > X[:,1]] = [0,0,1]
 labels[X[:, 0] <= X[:,1]] = [1,0,0]
 labels[X[:,1] + X[:, 0] > 1] = [0, 1, 0]
 
-x = K.placeholder(shape=(None, 2))
-t = K.placeholder(shape=(None, 3))
-
 initializer = RandomNormal(mean=0.0, stddev=0.01, seed=None)
 
 model = Sequential()
@@ -24,16 +21,6 @@ model.add(Dense(3, kernel_initializer=initializer, activation='softmax'))
 optimizer = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
-
-model.fit(X, labels, batch_size=20, epochs=1, show_accuracy=True)
-
-f = K.function([x], [K.argmax(forward(x),axis=1)])
-accuracy = 0
-for i in range(1000):
-    sample = X[batch_size*i:batch_size*(i+1)]
-    target = labels[batch_size*i:batch_size*(i+1)]
-    tt = f([sample])[0]
-    accuracy += np.sum(tt == np.argmax(target, axis=1))
-
-print("Accuracy", accuracy / 1000. /batch_size)
+model.fit(X, labels, batch_size=20, epochs=1)
+model.evaluate(X, labels)
 # accuracy 99.44
