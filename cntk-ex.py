b53c1668ab30e532769dc0ed44786f95706d6f62
chris <cbo@ivalua.com>
2019-03-21 19:10:06 +0100

    n

--- cntk-ex.py
+++ cntk-ex.py
@@ -8,16 +8,16 @@ labels[X[:, 0] > X[:,1]] = [0,0,1]
 labels[X[:, 0] <= X[:,1]] = [1,0,0]
 labels[X[:,1] + X[:, 0] > 1] = [0, 1, 0]
 
-x = C.input_variable(shape=( 2,), needs_gradient=False)
+x = C.input_variable(shape=(2,), needs_gradient=False)
 t = C.input_variable(shape=(3,), needs_gradient=False)
 
 init = C.initializer.normal(0.01)
 
 theta1 = C.Parameter(shape=(2, 12), init=init )
-bias1 = C.Parameter(shape=(1, 12,), init=init )
+bias1 = C.Parameter(shape=(12,), init=init )
 
 theta2 = C.Parameter(shape=(12,3), init=init )
-bias2 = C.Parameter(shape=(1, 3,), init=init )
+bias2 = C.Parameter(shape=(3,), init=init )
 
 def forward(x):
     y = C.times(x, theta1) + bias1
@@ -51,12 +51,12 @@ for i in range(min(dataset_size, 100000) // batch_size ):
     loss = y.eval({x:sample, t:target})
     print("cost {} - learning rate {}".format(loss[0], lr))
 
-y = C.squeeze(C.argmax(forward(x), 1),1)
+y = C.argmax(forward(x))
 accuracy = 0
 for i in range(1000):
     sample = X[batch_size*i:batch_size*(i+1)]
     target = labels[batch_size*i:batch_size*(i+1)]
-    tt = y.eval({x:sample})[0]
+    tt = y.eval({x:sample})
     accuracy += np.sum(tt == np.argmax(target, axis=1))
 
 print("Accuracy", accuracy / 1000. /batch_size)
