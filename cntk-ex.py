722031c777f53a8b4af5530831ef9c144b61bd38
chris <cbo@ivalua.com>
2019-03-22 10:10:26 +0100

    n

--- cntk-ex.py
+++ cntk-ex.py
@@ -8,9 +8,6 @@ labels[X[:, 0] > X[:,1]] = [0,0,1]
 labels[X[:, 0] <= X[:,1]] = [1,0,0]
 labels[X[:,1] + X[:, 0] > 1] = [0, 1, 0]
 
-x = C.input_variable(shape=(-1, 2), needs_gradient=False)
-t = C.input_variable(shape=(-1, 3), needs_gradient=False)
-
 init = C.initializer.normal(0.01)
 
 theta1 = C.Parameter(shape=(2, 12), init=init )
@@ -18,23 +15,6 @@ bias1 = C.Parameter(shape=(1, 12), init=init )
 
 theta2 = C.Parameter(shape=(12,3), init=init )
 bias2 = C.Parameter(shape=(1, 3,), init=init )
-
-def forward(x):
-    y = C.times(x, theta1) + bias1
-    y = C.element_max(y, 0.)
-    return C.times(y, theta2) + bias2
-
-def softmax(x):
-    e = C.exp(x)
-    s = C.reduce_sum(e, axis=1)
-    return e/s
-
-def crossentropy(y, t):
-    prob = C.squeeze(C.reduce_sum(y*t, axis=1), 1)
-    return - C.reduce_mean(C.log(prob))
-
-#y = C.reduce_mean(C.cross_entropy_with_softmax(forward(x), t, axis=1))
-y = crossentropy(softmax(forward(x)),t)
 
 x1 = C.input_variable(shape=(2,), needs_gradient=False)
 t1 = C.input_variable(shape=(3,), needs_gradient=False)
@@ -69,7 +49,7 @@ for i in range(min(dataset_size, 100000) // batch_size ):
     # print(forward(x).eval({x:sample}).shape, forward1(x1).eval({x1:sample}).shape)
     for param,grad in g.items():
         param.value = param.value - grad * lr
-    loss = y.eval({x:sample, t:target})
+    loss = y1.eval({x1:sample, t1:target})
     print("cost {} - learning rate {}".format(loss[0], lr))
 
 y = C.squeeze(C.argmax(forward(x), 1),1)
