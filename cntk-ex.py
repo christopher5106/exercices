b8f64f0aa154d4197618c02bcb5ad2b90d12c98a
chris <cbo@ivalua.com>
2019-03-22 09:31:23 +0100

    n

--- cntk-ex.py
+++ cntk-ex.py
@@ -39,9 +39,9 @@ y = crossentropy(softmax(forward(x)),t)
 x1 = C.input_variable(shape=(2,), needs_gradient=False)
 t1 = C.input_variable(shape=(3,), needs_gradient=False)
 def forward1(x):
-    y = C.times(x, theta1) + bias1
+    y = C.times(x, theta1) + bias1[0]
     y = C.element_max(y, 0.)
-    return C.times(y, theta2) + bias2
+    return C.times(y, theta2) + bias2[0]
 
 def softmax1(x):
     e = C.exp(x)
@@ -59,7 +59,7 @@ for i in range(min(dataset_size, 100000) // batch_size ):
     sample = X[batch_size*i:batch_size*(i+1)]
     target = labels[batch_size*i:batch_size*(i+1)]
     g = y.grad({x:sample, t:target}, wrt=[theta1, bias1, theta2, bias2])
-    g1 = y1.grad({x1:sample, t1:target}, wrt=[theta1, bias1, theta2, bias2])
+    # g1 = y1.grad({x1:sample, t1:target}, wrt=[theta1, bias1, theta2, bias2])
     print("g", forward(x).eval({x:sample}))
     print("g1", forward1(x1).eval({x1:sample}))
     print(forward(x).eval({x:sample}).shape, forward1(x1).eval({x1:sample}).shape)
