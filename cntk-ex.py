6f3da2202a088c6ad9d408b1cd3b2fda4ec7a8bc
chris <cbo@ivalua.com>
2019-03-22 09:36:54 +0100

    n

--- cntk-ex.py
+++ cntk-ex.py
@@ -60,7 +60,7 @@ for i in range(min(dataset_size, 100000) // batch_size ):
     target = labels[batch_size*i:batch_size*(i+1)]
     g = y.grad({x:sample, t:target}, wrt=[theta1, bias1, theta2, bias2])
     # g1 = y1.grad({x1:sample, t1:target}, wrt=[theta1, bias1, theta2, bias2])
-    print("g", (C.times(x1, theta1)++ bias1[0]).eval({x1:sample}), (C.times(x1, theta1)+ bias1[0]).eval({x1:sample}).shape)
+    print("g", (C.times(x1, theta1)+ bias1[0]).eval({x1:sample}), (C.times(x1, theta1)+ bias1[0]).eval({x1:sample}).shape)
     print("g", forward(x).eval({x:sample}))
     print("g1", forward1(x1).eval({x1:sample}))
     print(forward(x).eval({x:sample}).shape, forward1(x1).eval({x1:sample}).shape)
