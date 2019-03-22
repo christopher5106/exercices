9d424736045b6043632691427109e154729636d7
chris <cbo@ivalua.com>
2019-03-22 09:09:04 +0100

    n

--- cntk-ex_with_packages.py
+++ cntk-ex_with_packages.py
@@ -21,7 +21,7 @@ y = C.cross_entropy_with_softmax(z(x),t)
 
 batch_size = 20
 from cntk.learners import sgd, learning_parameter_schedule
-lr = learning_parameter_schedule([.5 *(.1**i) for i in range(10000)], minibatch_size=batch_size, epoch_size=1000)
+lr = learning_parameter_schedule([.5 *(.1**i) for i in range(10000)], minibatch_size=batch_size, epoch_size=1000*batch_size)
 learner = sgd(z.parameters, lr)
 
 for i in range(min(dataset_size, 100000) // batch_size ):
